ebefe88f105f37adc80eeb3a28b43b2f9a2adca6
Aryaman <someoneg4@gmail.com>
2022-12-19 03:51:31 +0530

    whoops

--- q1.c
+++ q1.c
@@ -10,7 +10,7 @@ pthread_mutex_t locks[5];
 int pickFork(int i) {
     // printf("Locking %d | Status: %d\n", i, pthread_mutex_trylock(&locks[i]));
     pthread_mutex_trylock(&locks[i]);
-    printf("----Fork %d picked----", i);
+    printf("----Fork %d picked----\n", i);
     if (forks[i]!=1) {
         forks[i] = 1;
         return 0;
@@ -37,7 +37,7 @@ int putFork(int i) {
         result = -1;
     }
 
-    printf("----Fork %d released----", i);
+    printf("----Fork %d released----\n", i);
     pthread_mutex_unlock(&locks[i]);
     return result;
 }
