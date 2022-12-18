42860d48f5ccc633d37bc183abdc3f194b042328
Aryaman <someoneg4@gmail.com>
2022-12-19 04:11:48 +0530

    yiek

--- q1.c
+++ q1.c
@@ -52,13 +52,13 @@ void *philosphise(void *_i) {
             pickFork(i);
             pickFork((i+1)%5);
             printf("+P%d eating (forks %d and %d)!\n", i, i, (i+1)%5);
-            printf("[%d, %d, %d, %d, %d]", forks[0], forks[1], forks[2], forks[3], forks[4]);
+            printf("[%d, %d, %d, %d, %d]\n", forks[0], forks[1], forks[2], forks[3], forks[4]);
             eat(i);
             sleep(1);
             printf("-P%d finish (forks %d and %d)!\n", i, i, (i+1)%5);
             putFork((i+1)%5);
             putFork(i);
-            printf("[%d, %d, %d, %d, %d]", forks[0], forks[1], forks[2], forks[3], forks[4]);
+            printf("[%d, %d, %d, %d, %d]\n", forks[0], forks[1], forks[2], forks[3], forks[4]);
             think(i);
         } else {
             pickFork((i+1)%5);
