60c30314446174d3d02bfdcc2fcc746a64a80ead
Aryaman <someoneg4@gmail.com>
2022-12-19 13:03:40 +0530

    dbg

--- q1.c
+++ q1.c
@@ -74,9 +74,9 @@ void *philosphise(void *_i) {
             eat(i);
             sleep(1);
             printf("--P%d finish (forks %d and %d)!\n", i, i, (i+1)%5);
-            printf("--[%d, %d, %d, %d, %d]\n", forks[0], forks[1], forks[2], forks[3], forks[4]);
             putFork((i+1)%5);
             putFork(i);
+            printf("--[%d, %d, %d, %d, %d]\n", forks[0], forks[1], forks[2], forks[3], forks[4]);
             think(i);
         }
     }
