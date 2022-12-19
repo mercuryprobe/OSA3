b64383cacd1e458605283d3fb9744d2cd814e125
Aryaman <someoneg4@gmail.com>
2022-12-19 13:00:01 +0530

    cleanup

--- q1.c
+++ q1.c
@@ -57,24 +57,24 @@ void *philosphise(void *_i) {
         if (i<4) {
             pickFork(i);
             pickFork((i+1)%5);
-            printf("+P%d eating (forks %d and %d)!\n", i, i, (i+1)%5);
-            printf("[%d, %d, %d, %d, %d]\n", forks[0], forks[1], forks[2], forks[3], forks[4]);
+            printf("++P%d eating (forks %d and %d)!\n", i, i, (i+1)%5);
+            printf("++[%d, %d, %d, %d, %d]\n", forks[0], forks[1], forks[2], forks[3], forks[4]);
             eat(i);
             sleep(1);
-            printf("-P%d finish (forks %d and %d)!\n", i, i, (i+1)%5);
+            printf("--P%d finish (forks %d and %d)!\n", i, i, (i+1)%5);
             putFork(i);
             putFork((i+1)%5);
-            printf("[%d, %d, %d, %d, %d]\n", forks[0], forks[1], forks[2], forks[3], forks[4]);
+            printf("--[%d, %d, %d, %d, %d]\n", forks[0], forks[1], forks[2], forks[3], forks[4]);
             think(i);
         } else {
             pickFork((i+1)%5);
             pickFork(i);
-            printf("+P%d eating (forks %d and %d)!\n", i, i, (i+1)%5);
-            printf("[%d, %d, %d, %d, %d]\n", forks[0], forks[1], forks[2], forks[3], forks[4]);
+            printf("++P%d eating (forks %d and %d)!\n", i, i, (i+1)%5);
+            printf("++[%d, %d, %d, %d, %d]\n", forks[0], forks[1], forks[2], forks[3], forks[4]);
             eat(i);
             sleep(1);
-            printf("-P%d finish (forks %d and %d)!\n", i, i, (i+1)%5);
-            printf("[%d, %d, %d, %d, %d]\n", forks[0], forks[1], forks[2], forks[3], forks[4]);
+            printf("--P%d finish (forks %d and %d)!\n", i, i, (i+1)%5);
+            printf("--[%d, %d, %d, %d, %d]\n", forks[0], forks[1], forks[2], forks[3], forks[4]);
             putFork((i+1)%5);
             putFork(i);
             think(i);
