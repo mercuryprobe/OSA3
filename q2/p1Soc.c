ee26797f7da6e4e8925d6d7b80beae09694db71f
Aryaman <someoneg4@gmail.com>
2022-12-20 16:48:42 +0530

    bruh

--- q2/p1Soc.c
+++ q2/p1Soc.c
@@ -49,7 +49,10 @@ int main() {
     }
 
     int cfiledescriptor = accept(sfiledescriptor, NULL, NULL);
-
+    if (cfiledescriptor<0) {
+        puts("[SERVER] Client accept failed!");
+        return 0;
+    }
     int i  = 0;    
     while (i<50) {
 
