820143c3957056d2e3b31e5a6cb49bac85e2ab5c
Aryaman <someoneg4@gmail.com>
2022-12-21 02:26:33 +0530

    aight

---
+++ q1a11.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <signal.h>
+
+int philosopher[5] = {0, 0, 0, 0, 0}; //default: thinking
+int forks[5] = {-1, -1, -1, -1, -1};
+pthread_mutex_t locks[5];
+
+static volatile sig_atomic_t active = 1;
+static void interrupter(int x) {
+    active = 0;
+}
+
+int pickFork(int i, int philNum) {
+    // printf("Locking %d | Status: %d\n", i, pthread_mutex_trylock(&locks[i]));   
+    // printf("----Fork %d picked----\n", i);
+    if ((forks[i]==-1) || ((forks[i]==-1) && ((philosopher[forks[i]]==0) || (philNum==forks[i])))) {
+        // if fork is unused or philosopher last using the fork has returned to thinking 
+        // or if the philosopher last using the fork was the current one
+        philosopher[philNum]=1; //acquiring a fork makes the philosopher hungry
+        forks[i]=philNum; //fork is labelled to be in use by current philosopher
+        return 0;
+    } else {
+        return -1;
+    }
+}
+
+void eat(int i) {
+    philosopher[i] = 2;
+}
+
+void think(int i) {
+    philosopher[i] = 0;
+}
+
+
+int putFork(int i) {
+    // doesn't do anything. 
+    // the lesser the interaction with forks[], the lesser the chances of conflict
+    // returning to thinking will allow other philosophers to take the fork
+}
+
+
+void *philosphise(void *_i) {
+    int i = *((int *) _i);
+
+    while (active) {
+        if (i<4) {
+            if (pickFork(i, i)==-1) {continue;}
+            if (pickFork((i+1)%5, i)==-1) {continue;}
+            philosopher[i]=2;
+            eat(i);
+            printf("Philosopher %d eating [%d::%d]!\n", i, i, (i+1)%5);
+            sleep(1);
+            think(i);
+            printf("Philosopher %d think_ [%d::%d]!\n", i, i, (i+1)%5);
+        } else {
+            if (pickFork((i+1)%5, i)==-1) {continue;}
+            if (pickFork(i, i)==-1) {continue;}
+            philosopher[i]=2;
+            eat(i);
+            printf("Philosopher %d eating [%d::%d]!\n", i, i, (i+1)%5);
+            sleep(1);
+            think(i);
+            printf("Philosopher %d think_ [%d::%d]!\n", i, i, (i+1)%5);
+        }
+    }
+}
+
+int main() {
+    pthread_t pids[5];
+    
+    signal(SIGINT, interrupter);
+    for (int i=0; i<5; i++) {
+        int *_i = malloc(sizeof(*_i));
+        *_i = i;
+        printf("thread started: %d\n", i);
+        pthread_create(&pids[i], NULL, &philosphise, _i);
+    }
+
+    for (int i=0; i<5; i++) {
+        pthread_join(pids[i], NULL);
+    }
+}
