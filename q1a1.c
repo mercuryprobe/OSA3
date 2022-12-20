b18b76545ec9083cf83f72731b343130b5c25da5
Aryaman <someoneg4@gmail.com>
2022-12-21 02:54:36 +0530

    okay

--- q1a1.c
+++ q1a1.c
@@ -6,6 +6,8 @@
 
 int philosopher[5] = {0, 0, 0, 0, 0}; //default: thinking
 int forks[5] = {-1, -1, -1, -1, -1};
+int l3[3] = {-1, -1, -1};
+int maintainer=0;
 
 static volatile sig_atomic_t active = 1;
 static void interrupter(int x) {
@@ -26,6 +28,8 @@ int pickFork(int i, int philNum) {
 
 void eat(int i) {
     philosopher[i] = 2;
+    int curMain = maintainer++;
+    l3[curMain%3];
 }
 
 void think(int i) {
@@ -40,11 +44,20 @@ int putFork() {
     // fork doesn't inherently need to store the state of use, only the user
 }
 
+int l3Check(int j) {
+    // checks if more than 1 of the last two eatings were of the same philosopher
+    int num;
+    for (int i =0; i<3; i++) {
+        if (j==l3[i]) {num++;}
+    }
+    return -(num>1); 
+}
 
 void *philosphise(void *_i) {
     int i = *((int *) _i);
-
+    
     while (active) {
+        if (l3Check==-1) {continue;}
         if (i<4) {
             // printf("%d :: [%d %d %d %d %d]\n", i, philosopher[0], philosopher[1], philosopher[2], philosopher[3], philosopher[4]);
             if (pickFork(i, i)==-1) {continue;}
