620b06030218623ba8507d2cf843329fa652d558
Aryaman <someoneg4@gmail.com>
2022-12-21 05:28:44 +0530

    welp

--- q2/shm/p1.c
+++ q2/shm/p1.c
@@ -31,7 +31,7 @@ int main() {
     struct timespec start;
     struct timespec stop;
     double billion = 1000000000;
-    sem_t *sem = sem_open(semLocation, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR, 1);
+    sem_t *sem = sem_open(semLocation, O_RDWR, S_IRUSR | S_IWUSR, 1);
 
     constructor();
     
