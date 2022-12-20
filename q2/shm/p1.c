d2a58b12778717ff9d4f077912e313f8afa5d9d5
Aryaman <someoneg4@gmail.com>
2022-12-20 21:13:18 +0530

    phatega

---
+++ q2/shm/p1.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/un.h>
+#include <time.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+
+char characters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char strings[50][5];
+
+
+void constructor() {
+    for (int i =0; i<50; i++) {
+        for (int j = 0; j<4; j++) {
+            strings[i][j] = characters[rand()%52];
+        }
+    }
+}
+
+int main() {
+    // reference: The Linux Programming Interface, Michael Kerrisk
+    const char *location = "/tmp/sharedmem";
+    struct timespec start;
+    struct timespec stop;
+    double billion = 1000000000;
+    
+    constructor();
+    
+    // https://man7.org/linux/man-pages/man3/shm_open.3.html, example
+    int filedescriptor = shm_open(location, O_CREAT | O_EXCL | O_RDWR, S_IRUSR | S_IWUSR);
+
+    if (ftruncate(filedescriptor, 2048) == -1) {
+        perror("[SERVER] Ftruncate failed!");
+    }
+    char *ptr = mmap(0, SHM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, filedescriptor, 0);
+    if (ptr == MAP_FAILED) {
+        perror("[SERVER] Mapping shm object failed!");
+        exit(1);
+    }
+
+    int i  = 0;
+    if (mkfifo(location, S_IRUSR | S_IWUSR | S_IWGRP)<0) {
+        perror("FIFO File Creation failed");
+    }
+    clock_gettime(CLOCK_REALTIME, &start);    
+    while (i<50) {
+        char curString[64];
+        const char space[2] = " ";
+        int j = i+5;
+        for (i; i<(j); i++) {
+            // reference: https://stackoverflow.com/questions/8257714/how-to-convert-an-int-to-string-in-c
+            int length = snprintf(NULL, 0, "%d", i);
+            char* numStr = malloc(length + 1);
+
+            snprintf(numStr, length+1, "%d", i);
+            strcat(curString, numStr);       //add integer
+            strcat(curString, space);        //add space
+            strcat(curString, strings[i]);   //add current string
+            strcat(curString, space);        //add space
+        }
+        // puts(curString);
+        
+        int out = open(location, O_WRONLY);
+        if ((write(out, curString, sizeof(curString)))<0) {
+            perror("[SERVER] FIFO write failed");
+            return 0;
+        }
+        close(out);
+        // printf("[SERVER] Characters written: %d\n", charWritten);
+        
+        int in = open(location, O_RDONLY);
+        char received[64];
+        if (read(in, received, 64)<0) {
+            perror("[SERVER] FIFO read failed");
+        };
+        // printf("[SERVER] Sent: %s\n", curString);
+        printf("[SERVER] Received index: %s\n\n", received);
+        for (int k =0; k<56; k++) {
+            curString[k] = 0;
+        }
+        close(in);
+    }
+    
+    clock_gettime(CLOCK_REALTIME, &stop);
+    double duration = stop.tv_sec + stop.tv_nsec/billion - (start.tv_sec + start.tv_nsec/billion);
+
+    printf("FIFO runtime: %lf\n", duration);
+    return 0;
+}
---
+++ q3/kernelmod.c
@@ -0,0 +1,43 @@
+// references: 
+// 1. The Linux Kernel Module Programming Guide, Peter Jay Salzman, Michael Burian, Ori Pomerantz, Bob Mottram, Jim Huang
+// 2. https://stackoverflow.com/questions/8547332/efficient-way-to-find-task-struct-by-pid
+// 3. https://github.com/torvalds/linux/blob/master/include/linux/sched.h
+
+// #define _XOPEN_SOURCE_EXTENDED 1
+// #include <unistd.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/pid.h>
+#include <linux/pid_namespace.h>
+#include <linux/cred.h>
+
+static int pidInt;
+static struct pid *pidStruct;
+struct task_struct *curTask;
+
+module_param(procpid, int, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+
+int init_module(void) {
+    pr_info("Hello world\n");
+
+    structpid = find_get_pid(pidInt);
+    if (structpid==NULL) {
+        pr_info("Invalid PID\n");
+        return 0;
+    }
+
+    curTask = pid_task(pid_struct, PIDTYPE_PID);
+    if (curTask==NULL) {
+        pr_info("Unexpected error occured.\n");
+        return 0;
+    }
+
+    pr_info("PID: %d\n", curTask.pid);
+    pr_info("User ID: %d\n", curTask.cred.uid);
+    pr_info("Process group ID: %d\n", pid_vnr(task_pgrp(curTask))); //task_tgid_nr(current)
+    pr_info("Process command path: %s\n", curTask.comm);
+
+    return 0;
+}
+
+MODULE_LICENSE("GPL");
