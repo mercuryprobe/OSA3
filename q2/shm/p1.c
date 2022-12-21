70dd86e6eb670704b96e97366e56fe72d73fdebf
Aryaman <someoneg4@gmail.com>
2022-12-21 15:35:21 +0530

    yikes

--- q2/shm/p1.c
+++ q2/shm/p1.c
@@ -89,7 +89,7 @@ int main() {
         memcpy(currentVal, pointer, sizeof("-"));
         while((strcmp(currentVal, "-")==0)) {
             memcpy(currentVal, pointer, sizeof("-"));
-            usleep(2);
+            usleep(1);
         };
         
         // int curSemV;
